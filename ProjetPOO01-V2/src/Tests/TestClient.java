a003b49f900339771707e25ff33827276a037acb
Gaied <34006535+Gaied@users.noreply.github.com>
2019-12-04 17:29:54 +0100

    Update TestClient
    Ajouter une liste d'achat

--- ProjetPOO01-V2/src/Tests/TestClient.java
+++ ProjetPOO01-V2/src/Tests/TestClient.java
@@ -2,10 +2,14 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import ProjetPOO01.GestionPersonnes.Client;
+import achat.Achat;
 
 public class TestClient {
 private Client c;
@@ -17,9 +21,15 @@ private Client c;
 	private final String ocodepostal="69100";
 	private final String onumclient="007";
 	
+	private final Date d = new Date();
+	private final ArrayList<Achat> ListAchat = new ArrayList<Achat>();
+	private  Achat a = new Achat(d, "Monteau", 7);
+	
+	
 	@Before
 	public void init() {
 		c = new Client(onom, oprenom, oadresse, oville, ocodepostal, onumclient);
+		ListAchat.add(a);
 	}
 
 	@Test
@@ -39,6 +49,12 @@ private Client c;
 		assertEquals(onumclient,c.getNumClient());
 	}
 	
+	@Test
+	public void testAchete() {
+		assertNotNull(a);
+		
+	}
+	
 	/*@Test
 	public void testLivre() {
 		fail("Not yet implemented");
@@ -51,11 +67,6 @@ private Client c;
 
 	@Test
 	public void testPaie() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAchete() {
 		fail("Not yet implemented");
 	}
 
